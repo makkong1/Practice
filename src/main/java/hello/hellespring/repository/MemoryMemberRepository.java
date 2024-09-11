8b6ed022431c3169cf45b039ac6ebde14d496184
pyb <wowong123@naver.com>
2024-09-11 16:39:08 +0900

    commit

--- src/main/java/hello/hellespring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellespring/repository/MemoryMemberRepository.java
@@ -7,7 +7,6 @@ import java.util.*;
 /**
  * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
-
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
@@ -38,4 +37,5 @@ public class MemoryMemberRepository implements MemberRepository{
     public void clearStore(){
         store.clear();
     }
+
 }
